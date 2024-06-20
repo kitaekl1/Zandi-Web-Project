64e26237689a84993870a976e125db770f340bb2
tjdxodns <167972733+tjdxodns@users.noreply.github.com>
2024-06-20 09:52:42 +0900

    Update CfmemberRepository.java

--- zandi/src/main/java/com/assey/zandi/repository/CfmemberRepository.java
+++ zandi/src/main/java/com/assey/zandi/repository/CfmemberRepository.java
@@ -1,10 +1,16 @@
 package com.assey.zandi.repository;
 
+import com.assey.zandi.account.CfmemberVO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import com.assey.zandi.account.cfmemberVO;
 
 @Repository
-public interface CfmemberRepository extends JpaRepository<cfmemberVO, String> {
-    boolean existsBymId(String mId);
+public interface CfmemberRepository extends JpaRepository<CfmemberVO, String> {
+    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END FROM CfmemberVO m WHERE m.mId = :mId")
+    boolean existsByMId(@Param("mId") String mId);
+
+    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END FROM CfmemberVO m WHERE m.mNickname = :mNickname")
+    boolean existsByMNickname(@Param("mNickname") String mNickname);
 }
