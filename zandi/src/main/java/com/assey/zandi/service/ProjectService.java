91900361199fc729c6fb760380cc05dde726e0fc
tjdxodns <167972733+tjdxodns@users.noreply.github.com>
2024-06-20 23:32:22 +0900

    Update ProjectService.java

--- zandi/src/main/java/com/assey/zandi/service/ProjectService.java
+++ zandi/src/main/java/com/assey/zandi/service/ProjectService.java
@@ -3,6 +3,9 @@ package com.assey.zandi.service;
 import com.assey.zandi.project.ProjectVO;
 import com.assey.zandi.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,7 +16,13 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public List<ProjectVO> searchProjects(String searchText) {
-        return projectRepository.findByPrNameContainingOrPrDescriptionContaining(searchText, searchText);
+    public List<ProjectVO> searchProjects(String searchText, int offset, int limit) {
+        Pageable pageable = PageRequest.of(offset / limit, limit);
+        Page<ProjectVO> projectPage = projectRepository.findByPrNameContainingOrPrDescriptionContaining(searchText, searchText, pageable);
+        return projectPage.getContent();
+    }
+
+    public int countSearchProjects(String searchText) {
+        return (int) projectRepository.countByPrNameContainingOrPrDescriptionContaining(searchText, searchText);
     }
 }
