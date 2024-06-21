3131e4187f3afc36194c510418dc14be985e7419
tjdxodns <167972733+tjdxodns@users.noreply.github.com>
2024-06-21 11:24:13 +0900

    Update ProjectController.java

--- zandi/src/main/java/com/assey/zandi/controller/ProjectController.java
+++ zandi/src/main/java/com/assey/zandi/controller/ProjectController.java
@@ -22,15 +22,24 @@ public class ProjectController {
     public String searchProjects(@RequestParam("searchText") String searchText,
                                  @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
                                  Model model) {
-        int pageSize = 1; // 페이지 당 프로젝트 수
+        int pageSize = 5; // 페이지 당 프로젝트 수
         int totalProjects = projectService.countSearchProjects(searchText); // 총 프로젝트 수
         int pageCount = (int) Math.ceil((double) totalProjects / pageSize);
 
         int offset = (pageNum - 1) * pageSize;
         List<ProjectVO> projectList = projectService.searchProjects(searchText, offset, pageSize);
+
+        // 페이징 그룹 설정
+        int pageGroupSize = 5;
+        int currentGroup = (int) Math.ceil((double) pageNum / pageGroupSize);
+        int groupStartPage = (currentGroup - 1) * pageGroupSize + 1;
+        int groupEndPage = Math.min(groupStartPage + pageGroupSize - 1, pageCount);
+
         model.addAttribute("projectList", projectList);
         model.addAttribute("currentPage", pageNum);
         model.addAttribute("pageCount", pageCount);
+        model.addAttribute("groupStartPage", groupStartPage);
+        model.addAttribute("groupEndPage", groupEndPage);
         model.addAttribute("searchText", searchText);
         return "zandiProject/projectList";
     }
